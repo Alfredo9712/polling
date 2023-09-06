5316106215ecd166a132f1a79a3e175e94e65767
Alfredo Mora <Alfredo9712mt@gmail.com>
2023-09-05 22:34:17 -0700

    refactor to use google ouath with next-router

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -47,5 +47,6 @@ export async function POST(request: Request) {
     secure: true,
     path: "/",
   });
+
   return authedResponse;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
+"use client";
+
 import Image from "next/image";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 import { Button } from "@/compononents/ui/button";
 
 export default function Home() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
-    <main className="=">
-      <Button variant={"destructive"}>Test</Button>
-    </main>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   );
 }
