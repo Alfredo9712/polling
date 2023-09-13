377721729db4e4e1239106704c15f37dae450be2
Alfredo Mora <Alfredo9712mt@gmail.com>
2023-09-12 20:16:27 -0700

    update copy and install nivo

--- src/app/(main)/polls/page.tsx
+++ src/app/(main)/polls/page.tsx
@@ -10,7 +10,7 @@ export default async function PollsPage() {
   return (
     <div className="mx-auto ">
       <div className="flex items-center justify-between mb-14">
-        <h1 className="font-medium text-lg">View active polls</h1>
+        <h1 className="font-medium text-lg">View public polls</h1>
         <Link
           href="/polls/add"
           className="bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90 rounded-md flex py-2 px-3 items-center gap-1"
